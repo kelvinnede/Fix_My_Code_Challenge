c76d8cfb62a9daf42ee6f68d0222d3aec8b879c2
Kelvin Nede <kelvinnede@gmail.com>
2024-03-21 04:39:48 -0700

    0

---
+++ 0-fizzbuzz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def fizzbuzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=' ')
+        elif i % 3 == 0:
+            print("Fizz", end=' ')
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+        else:
+            print(i, end=' ')
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: ./0-fizzbuzz.py <number>")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+        fizzbuzz(n)
+    except ValueError:
+        print("Error: Please provide a valid integer.")
+        sys.exit(1)
